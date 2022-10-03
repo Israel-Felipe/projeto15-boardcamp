86035e3b9b9dbc5b90ad05b8742b645f85309b5b
Israel-Felipe <israelfelipet@gmail.com>
2022-10-03 16:46:54 -0300

    division into routes

--- hotel-hilbert/database-join-7a7ce1c6/scripts/002-cria-clientes.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE "guests" (
-  "id" SERIAL PRIMARY KEY,
-  "name" TEXT NOT NULL
-);
--- hotel-hilbert/database-join-7a7ce1c6/scripts/003-cria-hospedagem.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE "allocations" (
-  "id" SERIAL PRIMARY KEY,
-  "roomId" INTEGER NOT NULL,
-  "guestId" INTEGER NOT NULL,
-  "startDate" DATE NOT NULL,
-  "endDate" DATE
-);
